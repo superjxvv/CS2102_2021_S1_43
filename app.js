a46103ee84c9fc7092a144805bea70b89cec0884
jeremy <j3r3my1996@gmail.com>
2020-10-11 18:41:24 +0800

    added preliminary list caretakers page

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
-const fs = require('fs');
-const express = require('express');
-const _ = require('lodash');
+const fs = require("fs");
+const express = require("express");
+const _ = require("lodash");
+const sql_query = require("./sql");
+const path = require("path");
+const { Pool } = require("pg");
+//create .env file (ignored by git for privacy) to store environment variables
+//etc. DATABASE_URL=postgres://<username>:<password>@localhost:5432/pet_care
+require("dotenv").config();
 
 const app = express();
 
 //register view engine
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 //Set default view directory as /views/
-app.set('views')
+app.set("views");
+//Set path for static files
+app.use(express.static(path.join(__dirname, "public")));
+//to parse response to json
+app.use(express.json());
 
 //DB connection
-const {Pool, Client} = require('pg');
-//'postgressql://<username>:<password>@<hostIP>:<portNo.>/<databaseName>'
-//PetCare is our schema/db name
-const connectionString = 'postgressql://postgre:password@localhost:5432/PetCare'
+//Use pool.query to run a query on the first available idle client
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
 
-client.connect()
+app.get("/caretaker", async (req, res) => {
+  try {
+    const allCareTaker = await pool.query(sql_query.query.all_caretaker);
+    res.render("search", { careTakers: allCareTaker.rows });
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
-//Use client.query to query.
+app.listen(3000, () => {
+  console.log("server has started on port 3000");
+});

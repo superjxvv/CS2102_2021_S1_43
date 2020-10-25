b0071fdc53323739fee4b989186b46c9c38813de
jlxw48 <jlxw48@gmail.com>
2020-10-25 16:58:50 +0800

    update transactions, profile as discussed

--- sql/index.js
+++ sql/index.js
@@ -16,17 +16,17 @@ sql.query = {
 
     // top 4 ratings
   caretaker_top_ratings:
-    'SELECT name, location, rating, job FROM care_taker WHERE location = $1 ORDER BY rating DESC LIMIT 4',
+    'SELECT name, location, rating, job, email FROM care_taker WHERE location = $1 ORDER BY rating DESC LIMIT 4',
   // 4 most recent transactions
   recent_trxn_po:
-    'SELECT H.hire_status, H.start_date, H.end_date, C.name AS ct_name, P.name AS po_name, H.pet_name, H.rating, H.review_text FROM hire H INNER JOIN care_taker C ON H.ct_email = C.email INNER JOIN pet_owner P ON H.owner_email = P.email WHERE H.owner_email = $1 ORDER BY H.transaction_date DESC LIMIT 4',
+    'SELECT H.hire_status, H.start_date, H.end_date, C.name AS ct_name, C.email AS ct_email, P.name AS po_name, H.pet_name, H.rating, H.review_text FROM hire H INNER JOIN care_taker C ON H.ct_email = C.email INNER JOIN pet_owner P ON H.owner_email = P.email WHERE H.owner_email = $1 ORDER BY H.transaction_date DESC LIMIT 4',
   // 4 of my pets
   my_pets:
     'SELECT * FROM own_pet O INNER JOIN is_of I ON O.pet_name = I.pet_name AND O.email = I.owner_email WHERE O.email = $1 LIMIT 4',
   get_po_info: 'SELECT * FROM pet_owner WHERE email = $1',
   get_ct_info: 'SELECT * FROM care_taker WHERE email = $1',
-  get_my_trxn: "SELECT *, CASE WHEN H.hire_status = 'pendingAccept' OR H.hire_status = 'pendingPayment' THEN 1 ELSE 2 END AS button FROM hire H INNER JOIN care_taker C ON H.ct_email = C.email WHERE H.owner_email = $1 ORDER BY transaction_date DESC, start_date DESC, end_date DESC",
-  get_ct_email: 'SELECT C.name AS ct_name, C.email AS ct_email, H.hire_status, H.start_date, H.end_date, H.rating, H.review_text FROM care_taker C INNER JOIN hire H ON C.email = H.ct_email WHERE C.name = $1 ORDER BY H.transaction_date DESC LIMIT 4',
+  get_my_trxn: "SELECT C.email AS ct_email, C.name as ct_name, *, CASE WHEN H.hire_status = 'pendingAccept' OR H.hire_status = 'pendingPayment' THEN 1 ELSE 2 END AS button FROM hire H INNER JOIN care_taker C ON H.ct_email = C.email WHERE H.owner_email = $1 ORDER BY transaction_date DESC, start_date DESC, end_date DESC",
+  get_ct_trxn: 'SELECT C.name AS ct_name, C.email AS ct_email, H.hire_status, H.start_date, H.end_date, H.rating, H.review_text, P.name AS po_name, I.pet_type AS type, H.pet_name FROM care_taker C INNER JOIN hire H ON C.email = H.ct_email INNER JOIN pet_owner P ON P.email = H.owner_email INNER JOIN is_of I ON I.owner_email = P.email WHERE C.email = $1 ORDER BY H.transaction_date DESC LIMIT 4',
   get_a_hire: "SELECT * FROM hire WHERE owner_email = $1 AND ct_email = $2 AND start_date = $3 AND end_date = $4 AND pet_name = $5",
   get_ct_type: "SELECT job FROM care_taker WHERE email = $1",
   dates_caring: "SELECT start_date, end_date FROM hire WHERE ct_email = $1",

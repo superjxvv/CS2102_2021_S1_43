4c092b5be0709593fcc7a6a4f5f9f16a186780d8
swayongshen <yongshenswa@gmail.com>
2020-10-28 18:52:32 +0800

    Added add_date trigger for any insertion into hire.
    Will automatically add (start_date, end_date) into date_range table if
    not already existing. Prevent foreign key error and also don't need to
    manually add the date in app.js

--- sql/postgre.sql
+++ sql/postgre.sql
@@ -3549,3 +3549,18 @@ INSERT INTO has_credit_card (number, email, expiry) VALUES (cc_num, po_email, cc
 END;
 '
 LANGUAGE plpgsql;
+
+--Add into dates to date_range if not exists to prevent foreign key error.
+CREATE OR REPLACE FUNCTION add_date() RETURNS TRIGGER AS 
+$$ 
+BEGIN 
+  IF ((NEW.start_date, NEW.end_date) NOT IN (SELECT * FROM date_range)) THEN 
+    INSERT INTO date_range(start_date, end_date) VALUES(NEW.start_date, NEW.end_date); 
+  END IF;
+  
+  RETURN NEW; 
+END; 
+$$ 
+LANGUAGE plpgsql;
+
+CREATE TRIGGER hire_add_date AFTER INSERT OR UPDATE ON hire FOR EACH ROW EXECUTE PROCEDURE add_date();
